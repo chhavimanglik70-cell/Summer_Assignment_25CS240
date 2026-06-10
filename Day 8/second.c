2c9ed6796bf80efc6c6df9485b5d588c49d28381
chhavi <chhavimanglik70@gmail.com>
2026-06-10 12:13:28 +0530

    Initial commit

---
+++ Day 8/second.c
@@ -0,0 +1,20 @@
+// Print number triangle
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+#include<stdio.h>
+int main(){
+    int i, j, n;
+    printf("Enter number of rows= ");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++){
+        for(j=1; j<=i; j++){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
