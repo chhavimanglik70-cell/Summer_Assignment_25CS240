e0d664a304eba0d0f9a499549a08ab8afce8932e
chhavi <chhavimanglik70@gmail.com>
2026-06-10 12:42:35 +0530

    Initial commit

---
+++ Day 9/third.c
@@ -0,0 +1,20 @@
+// Repeated character pattern 
+// A 
+// BB
+// CCC
+// DDDD
+// EEEEE 
+
+#include<stdio.h>
+int main(){ 
+    int i, j;
+char c='A';
+for(i=1; i<=5; i++){
+    for(j=1; j<=i; j++){
+        printf("%c", c);
+    }
+    c++;
+    printf("\n");
+}
+return 0;
+}
