16e566239f896e807dc90c7f4dcd6bd76850a73c
chhavi <chhavimanglik70@gmail.com>
2026-06-11 10:51:22 +0530

    Initial commit

---
+++ Day 11/fourth.c
@@ -0,0 +1,17 @@
+// Function to find factorial
+
+#include<stdio.h>
+int factorial(int n){
+    int i, fact=1;
+    for(i=1; i<=n; i++){
+        fact= fact*i;
+    }
+    return fact;
+}
+int main(){
+    int num;
+    printf("Enter a number= ");
+    scanf("%d", &num);
+    printf("Fcatorial= %d", factorial(num));
+    return 0;
+}
---
+++ Day 11/third.c
@@ -0,0 +1,25 @@
+// Function to check prime number
+
+#include<stdio.h>
+int fact(int n){
+    int i;
+    if(n <= 1)
+    return 0;
+    for(i=2; i<= n/2; i++){
+        if(n % i == 0)
+        return 0;
+    }
+    return 1;
+}
+int main(){
+    int n;
+    printf("Enter a number= ");
+    scanf("%d", &n);
+
+    if(fact(n))
+    printf("Prime number");
+    else
+    printf("Not a prime number");
+    
+    return 0;
+}
