55d6d07e5aca02f3ec688c1b31e05f58ff4c28cd
chhavi <chhavimanglik70@gmail.com>
2026-06-13 11:18:27 +0530

    INitial commit

---
+++ Day 13/first.c
@@ -0,0 +1,18 @@
+// Input and display array
+
+#include<stdio.h>
+int main(){
+    int arr[100], n, i;
+    printf("Enter number of elements= ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements= \n", n);
+    for(i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Array elements are= \n");
+    for(i=0; i<n; i++){
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
---
+++ Day 13/second.c
@@ -0,0 +1,20 @@
+// Find sum and average of an array
+
+#include<stdio.h>
+int main(){
+    int arr[100], n, i, sum=0;
+    float avg;
+    printf("Enter number of elements= ");
+    scanf("%d" ,&n);
+
+    printf("Enter %d elements= \n", n);
+    for(i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+    avg = (float)sum/n;
+    printf("Sum = %d\n", sum);
+    printf("Average= %f\n", avg);
+    
+    return 0;
+}
