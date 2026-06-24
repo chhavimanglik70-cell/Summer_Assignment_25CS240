0305890d56d1e8a8ded09cf83f0477404171db21
chhavi <chhavimanglik70@gmail.com>
2026-06-24 20:39:58 +0530

    Initial commit

---
+++ Day 24/fourth.c
@@ -0,0 +1,25 @@
+// Remove duplicate characters from the string
+
+
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i, j, k;
+    printf("Enter string= ");
+    scanf("%s", str);
+   
+    for(i=0; str[i] != '\0'; i++){
+        for(j=i+1; str[j] != '\0';){
+            if(str[i] == str[j]){
+                for(k=j; str[k] != '\0'; k++)
+                str[k] = str[k+1];
+            }
+            else{
+                j++;
+            }
+        }
+    }
+    printf("New string= %s", str);
+
+    return 0;
+}
